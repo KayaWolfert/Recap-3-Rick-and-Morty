c8b55568b252c56a74c4e3079774b49c193c86a6
Kaya Wolfert <kayawolfert@web.de>
2024-04-04 12:53:38 +0200

    make pagination work

--- index.js
+++ index.js
@@ -11,14 +11,16 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
+let maxPage = 1;
+let page = 1;
 const searchQuery = "";
 
 export async function fetchCharacters() {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
+  
+  const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
   const data = await response.json();
-  console.log(data.results);
+  maxPage = data.info.pages;
+  pagination.textContent = `Page ${page} of ${maxPage}`;
   return data.results;
 
 }
@@ -28,8 +30,26 @@ fetchDataAndRender();
 async function fetchDataAndRender() {
   const serienCharacters = await fetchCharacters();
   console.log(serienCharacters);
+  cardContainer.innerHTML = "";
   serienCharacters.forEach((character) => {
     const newCard = CharacterCard(character);
     cardContainer.append(newCard);
   });
 }
+
+nextButton.addEventListener("click" , () => {
+  if (page < maxPage){
+    page++;
+    fetchDataAndRender();
+  }
+
+
+});
+
+prevButton.addEventListener("click" , () => {
+  if (page > 1){
+    page--;
+    fetchDataAndRender();
+  }
+
+});
